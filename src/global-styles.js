61392eb62950eef655a072232213eba84303c90f
Hakan <hknakks1@gmail.com>
2022-03-04 21:03:05 +0300

    first commit

---
+++ src/global-styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html, body {
+    font-family:'Helvetica Neue',Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing:antialiased;
+    -moz-osx-font-smoothing:grayscale;
+    background-color: black;
+    color: #333333;
+}
+
+`;
+export default GlobalStyle;
